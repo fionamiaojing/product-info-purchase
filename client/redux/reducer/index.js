6f2a1e1934df41e9860cc0f16e248aa44988129a
fiona <fionamiaojing@gmail.com>
2018-06-07 19:32:07 -0700

    Change the reducer function of products and items to reduce real data

--- client/redux/reducer/index.js
+++ client/redux/reducer/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import groupItems from './products';
-import allItems from './allitems';
+import { groupItems } from './products';
+import { allItems } from './allitems';
 import { optionChoice, quantityChoice } from './userChoice';
 import { displayError } from './displayError';
 
