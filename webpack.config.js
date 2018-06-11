a9e3327966180e5eaad2b5e2ae4e2583fd5cd046
fiona <fionamiaojing@gmail.com>
2018-06-11 14:05:21 -0700

    change css style for add-to-cart button

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,13 @@ module.exports = {
         test: /\.css$/,
         use: [
           { loader: "style-loader" },
-          { loader: "css-loader" }
+          { loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: "[name]__[local]___[hash:base64:5]"
+            }
+          }
         ]
       },
     ],
