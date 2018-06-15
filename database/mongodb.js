9a2ae78cca511461633a6070e319940c02cfb48e
fiona <fionamiaojing@gmail.com>
2018-06-14 19:06:32 -0700

    use docker - part 2

--- database/mongodb.js
+++ database/mongodb.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://172.17.0.2/16/etsy');
+//use this line if using docker-compose to build mongodb and component together
+mongoose.connect('mongodb://database/etsy');
+//use this line if using pre-built mongodb container
+//mongoose.connect('mongodb://172.17.0.2/16/etsy');
+//use this line if running localhost
 //mongoose.connect('mongodb://localhost/etsy');
 
 //GROUP ITEM----------------------------------------------------------------->
